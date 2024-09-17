a06a179db9ecd31135947750ee21dd06cf214b6b
Goodness Duru <goodyduru@gmail.com>
2024-09-17 19:22:17 +0100

    Add site and articles to db

--- static/main.js
+++ static/main.js
@@ -1,4 +1,10 @@
-const ARTICLE_LIST_HREF = "#article-list"
+const ARTICLE_LIST_HREF = "#article-list";
+const DB_NAME = "offline-rss";
+const DB_VERSION = 1;
+const SITE_STORE_NAME = "sites";
+const ARTICLE_STORE_NAME = "articles";
+
+let db;
 
 function addEventListeners() {
     document.querySelectorAll("aside a").forEach((anchor) => {
@@ -44,12 +50,12 @@ async function addFeed(evt) {
     const dom = new window.DOMParser().parseFromString(text, type);
     let feed = dom.querySelector("rss");
     if (feed != null) {
-        parseRSS(dom);
+        await parseRSS(dom);
         return;
     }
     feed = dom.querySelector("feed");
     if ( feed != null ) {
-        parseAtom(dom);
+        await parseAtom(dom);
     }
 }
 
@@ -74,17 +80,25 @@ async function fetchFeed(evt) {
     return response;
 }
 
-function parseRSS(dom) {
+async function parseRSS(dom) {
+    let feed = dom.querySelector("rss");
     let title = dom.querySelector("channel>title");
     let link = dom.querySelector("channel>link");
     let description = dom.querySelector("channel>description");
-    let result = {title: "", feed_link: "", site_link: "", description: "", entries: []};
+    let result = {title: "", feed_url: "", site_url: "", description: "", entries: []};
     result.title = ( title != null ) ? title.innerHTML : "";
-    result.feed_link = ( link != null ) ? link.innerHTML : "";
+    result.feed_url = ( link != null ) ? link.innerHTML : "";
     result.description = ( description != null ) ? description.innerHTML : "";
+    let site = {title: result.title, feed_url: result.feed_url, site_url: result.site_url, 
+        description: result.description, hash: cyrb53(feed.innerHTML)};
+    let site_id = await getOrCreateSite(site);
+    if ( site_id == 0 ) {
+        return;
+    }
     let items = dom.querySelectorAll("item");
+    let article_store = getObjectStore(ARTICLE_STORE_NAME, 'readwrite');
     items.forEach(item => {
-        entry = {title: "", link: "", content: "", pubDate: ""};
+        entry = {title: "", link: "", content: "", pubDate: "", hash: 0, site_id: site_id};
         let title = item.querySelector("title");
         let link = item.querySelector("link");
         let description = item.querySelector("description");
@@ -93,30 +107,40 @@ function parseRSS(dom) {
         entry.link = ( link != null ) ? link.innerHTML : "";
         entry.content = ( description != null ) ? description.innerHTML : "";
         entry.pubDate = ( pubDate != null ) ? pubDate.innerHTML : "";
+        entry.hash = cyrb53(item.innerHTML);
         result.entries.push(entry);
+        getOrCreateArticle(article_store, entry);
     });
     printFeed(result);
 }
 
-function parseAtom(dom) {
+async function parseAtom(dom) {
+    let feed = dom.querySelector("feed");
     let title = dom.querySelector("feed>title");
     let links = dom.querySelectorAll("feed>link");
-    let result = {title: "", feed_link: "", site_link: "", description: "", entries: []};
+    let result = {title: "", feed_url: "", site_url: "", description: "", entries: []};
     result.title = ( title != null ) ? title.innerHTML : "";
     if ( links.length == 1 ) {
-        result.feed_link = links[0].getAttribute("href");
+        result.feed_url = links[0].getAttribute("href");
     } else {
         links.forEach((link) => {
             if ( link.getAttribute("rel") == "self" ) {
-                result.feed_link = link.getAttribute("href");
+                result.feed_url = link.getAttribute("href");
             } else {
-                result.site_link = link.getAttribute("href");
+                result.site_url = link.getAttribute("href");
             }
         })
     }
+    let site = {title: result.title, feed_url: result.feed_url, site_url: result.site_url, 
+        description: result.description, hash: cyrb53(feed.innerHTML)};
+    let site_id = await getOrCreateSite(site);
+    if ( site_id == 0 ) {
+        return;
+    }
     let items = dom.querySelectorAll("entry");
+    let article_store = getObjectStore(ARTICLE_STORE_NAME, 'readwrite');
     items.forEach(item => {
-        entry = {title: "", link: "", description: "", pubDate: ""};
+        entry = {title: "", link: "", content: "", pubDate: "", hash: 0, site_id: site_id};
         let title = item.querySelector("title");
         let link = item.querySelector("link");
         let content = item.querySelector("content");
@@ -125,7 +149,9 @@ function parseAtom(dom) {
         entry.link = ( link != null ) ? link.getAttribute("href") : "";
         entry.content = ( content != null ) ? content.innerHTML : "";
         entry.pubDate = ( pubDate != null ) ? pubDate.innerHTML : "";
+        entry.hash = cyrb53(item.innerHTML);
         result.entries.push(entry);
+        getOrCreateArticle(article_store, entry);
     });
     printFeed(result);
 }
@@ -136,8 +162,8 @@ function printFeed(feed) {
         html += `<h2>${feed.title}</h2>`;
     }
 
-    if ( feed.site_link != "" ) {
-        html += `<p>Visit the site link: <a href="${feed.site_link}">${feed.site_link}</a></p>`;
+    if ( feed.site_url != "" ) {
+        html += `<p>Visit the site link: <a href="${feed.site_url}">${feed.site_url}</a></p>`;
     }
     if ( feed.description != "" ) {
         html += `<div>${feed.description}</div>`;
@@ -168,5 +194,96 @@ function printFeed(feed) {
     showOneMain(ARTICLE_LIST_HREF);
 }
 
+/**
+ * Hash function by bryc
+ * https://github.com/bryc/code/blob/master/jshash/experimental/cyrb53.js
+ */
+const cyrb53 = function(str, seed = 0) {
+    let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+    for(let i = 0, ch; i < str.length; i++) {
+      ch = str.charCodeAt(i);
+      h1 = Math.imul(h1 ^ ch, 2654435761);
+      h2 = Math.imul(h2 ^ ch, 1597334677);
+    }
+    h1  = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
+    h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+    h2  = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
+    h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+    return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+};
+
+function openDB() {
+    let req = indexedDB.open(DB_NAME, DB_VERSION);
+    req.onsuccess = (evt) => {
+        db = evt.target.result;
+    };
+    req.onerror = (evt) => {
+        console.error("Error: ", evt.target.error?.message);
+    };
+    req.onupgradeneeded = (evt) => {
+        const site_store = evt.currentTarget.result.createObjectStore(SITE_STORE_NAME, 
+            {keyPath: 'id', autoIncrement: true});
+        const article_store = evt.currentTarget.result.createObjectStore(ARTICLE_STORE_NAME, 
+            {keyPath: 'id', autoIncrement: true});
+        site_store.createIndex('feed_url', 'feed_url', {unique: true});
+        site_store.createIndex('hash', 'hash', {unique: true});
+        article_store.createIndex('site_id', 'site_id', {unique: false});
+        article_store.createIndex('hash', 'hash', {unique: true});
+    }
+}
+
+function getObjectStore(store_name, mode) {
+    let tx = db.transaction(store_name, mode);
+    return tx.objectStore(store_name);
+}
+
+async function getOrCreateSite(site) {
+    return new Promise((resolve, reject) => {
+        let store = getObjectStore(SITE_STORE_NAME, 'readwrite');
+        let hashIndex = store.index('hash');
+        let id = 0;
+        let req = hashIndex.get(site.hash);
+        req.onsuccess = (evt) => {
+            if ( typeof evt.target.result == 'undefined' ) {
+                let addReq = store.add(site);
+                addReq.onsuccess = (addEvt) => {
+                    resolve(addEvt.target.result);
+                };
+                addReq.onerror = () => {
+                    reject(0);
+                }
+
+            } else {
+                id = evt.target.result.id;
+                resolve(id);
+            }
+        };
+        req.onerror = () => {
+            reject(0);
+        };
+    });
+}
+
+function getOrCreateArticle(store, article) {
+    let getReq = store.index('hash');
+    getReq.get(article.hash).onsuccess = (getEvt) => {
+        if ( typeof getEvt.target.result == 'undefined' ) {
+            addArticle(store, article);
+            return;
+        }
+    }
+}
+
+function addArticle(storeObject, article) {
+    let req = storeObject.add(article);
+    req.onsuccess = (evt) => {
+        console.log("Article added");
+    };
+    req.onerror = (evt) => {
+        console.error(evt.target.error);
+    };
+}
+
+openDB();
 showUrlMain();
 addEventListeners();
