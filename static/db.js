1d67d9991ce54bbbb69d2117161862ddac369e01
Goodness Duru <goodyduru@gmail.com>
2024-10-15 16:08:35 +0100

    WIP: Db refactor

--- static/db.js
+++ static/db.js
@@ -1,43 +1,223 @@
-const DB_NAME = "offline-rss";
-const DB_VERSION = 1;
-const SITE_STORE_NAME = "sites";
-const ARTICLE_STORE_NAME = "articles";
-const PER_PAGE = 50;
+/**
+ * This class deals with creating/opening a connection to the IndexedDB stores.
+ */
+app.DB = class Db {
+    constructor() {
+        this.siteStoreName = "sites";
+        this.articleStoreName = "articles";
+        this.db = null;
+    }
 
-let db;
+    async open() {
+        const DB_NAME = "offline-rss";
+        const DB_VERSION = 1;
+        return new Promise((resolve, reject) => {
+            let req = indexedDB.open(DB_NAME, DB_VERSION);
+            req.onsuccess = (evt) => {
+                this.db = evt.target.result;
+                resolve();
+            };
+            req.onerror = (evt) => {
+                console.error("Error: ", evt.target.error?.message);
+                reject();
+            };
+            req.onupgradeneeded = (evt) => {
+                const siteStore = evt.currentTarget.result.createObjectStore(this.siteStoreName, 
+                    {keyPath: 'id', autoIncrement: true});
+                const articleStore = evt.currentTarget.result.createObjectStore(this.articleStoreName, 
+                    {keyPath: 'id', autoIncrement: true});
+                siteStore.createIndex('feedUrl', 'feedUrl', {unique: true});
+                siteStore.createIndex('hash', 'hash', {unique: true});
+                siteStore.createIndex('nextPoll', 'nextPoll', {unique: false});
+                articleStore.createIndex('siteId', 'siteId', {unique: false});
+                articleStore.createIndex('hash', 'hash', {unique: true});
+                articleStore.createIndex('link', 'link', {unique: false});
+                articleStore.createIndex('isRead', 'isRead', {unique: false});
+                articleStore.createIndex('siteUnread', ['siteId', 'isRead'], {unique: false})
+            };
+        });
+    }
 
+    getArticleStore(mode) {
+        let tx = this.db.transaction(this.articleStoreName, mode);
+        return tx.objectStore(this.articleStoreName);
+    }
 
-function openDB() {
-    return new Promise((resolve, reject) => {
-        let req = indexedDB.open(DB_NAME, DB_VERSION);
-        req.onsuccess = (evt) => {
-            db = evt.target.result;
-            resolve();
-        };
-        req.onerror = (evt) => {
-            console.error("Error: ", evt.target.error?.message);
-            reject();
-        };
-        req.onupgradeneeded = (evt) => {
-            const siteStore = evt.currentTarget.result.createObjectStore(SITE_STORE_NAME, 
-                {keyPath: 'id', autoIncrement: true});
-            const articleStore = evt.currentTarget.result.createObjectStore(ARTICLE_STORE_NAME, 
-                {keyPath: 'id', autoIncrement: true});
-            siteStore.createIndex('feedUrl', 'feedUrl', {unique: true});
-            siteStore.createIndex('hash', 'hash', {unique: true});
-            siteStore.createIndex('nextPoll', 'nextPoll', {unique: false});
-            articleStore.createIndex('siteId', 'siteId', {unique: false});
-            articleStore.createIndex('hash', 'hash', {unique: true});
-            articleStore.createIndex('link', 'link', {unique: false});
-            articleStore.createIndex('isRead', 'isRead', {unique: false});
-            articleStore.createIndex('siteUnread', ['siteId', 'isRead'], {unique: false})
-        };
-    });
+    getSiteStore(mode) {
+        let tx = this.db.transaction(this.siteStoreName, mode);
+        return tx.objectStore(this.siteStoreName);
+    }
 }
 
-function getObjectStore(storeName, mode) {
-    let tx = db.transaction(storeName, mode);
-    return tx.objectStore(storeName);
+app.models.Site = class Site {
+
+    /**
+     * Gets a site from the Indexeddb store based on the unique attribute provided. The attribute provided
+     * has to be unique for this to work. The type of attribute determines the specific store index used to
+     * fetch the site object.
+     * @param {String} attrType The attribute type of the site object. This determines the store index to use.
+     * @param {String|Number} attrVal The key to identify the site object.
+     * @returns {Object} Returns the site object from the store.
+     */
+    async get(attrType, attrVal) {
+        return new Promise((resolve, reject) => {
+            const store = app.DB.getSiteStore('readonly');
+            let req;
+            if ( attrType == 'id' ) {
+                req = store.get(attrVal);
+            } else {
+                const index = store.index(attrType);
+                req = index.get(attrVal);
+            }
+            req.onsuccess = () => {
+                resolve(req.result);
+            };
+            req.onerror = () => {
+                console.error(req.error);
+                reject(null);
+            };
+        });
+    }
+
+    /**
+     * This function adds a site to the db store
+     * @param {Object} site object to add to the db store.
+     */
+    async add(site) {
+        return new Promise((resolve, reject) => {
+            const store = app.DB.getSiteStore('readwrite');
+            let req = store.add(site);
+            req.onerror = () => {
+                console.error(req.error);
+                reject();
+            };
+            req.onsuccess = () => {
+                site.id = req.result;
+                resolve();
+            }
+        });
+    }
+
+    /**
+     * Checks if a site object exists in the db.
+     * @param {String} attrType Determines the index to use. 'id' just uses the store directly since it's the primary key.
+     * @param {String|Number} attrVal The key to identify the object.
+     * @returns {bool} Does it exists.
+     */
+    async exists(attrType, attrVal) {
+        return new Promise((resolve, reject) => {
+            const store = app.DB.getSiteStore('readonly');
+            let req;
+            if ( attrType == 'id' ) {
+                req = store.get(attrVal);
+            } else {
+                let index = store.index(attrType);
+                req = index.get(attrVal);
+            }
+            req.onsuccess = () => {
+                if ( typeof req.result == 'undefined' ) {
+                    resolve(false);
+                } else {
+                    resolve(true);
+                }
+            }
+            req.onerror = () => {
+                console.error(req.error);
+                reject(true);
+            }
+        });
+    }
+
+    /**
+     * @param {Object} site Site object to update in db.
+     * @returns {bool} true if successful, false otherwise.
+     */
+    async update(site) {
+        return new Promise((resolve, reject) => {
+            const store = app.DB.getSiteStore("readwrite");
+            let req = store.put(site);
+            req.onsuccess = () => {
+                resolve(true);
+            };
+            req.onerror = () => {
+                console.error(req.error);
+                reject(false);
+            }
+        })
+    }
+
+    /**
+     * This gets all the sites whose polling time is less than the current time.
+     * @returns {Array} Returns an array of sites to poll.
+     */
+    async getToPoll() {
+        return new Promise((resolve, reject) => {
+            const store = app.DB.getSiteStore("readonly");
+            const currentTime = Date.now();
+            const timeRange = IDBKeyRange.upperBound(currentTime);
+            const index = store.index("nextPoll");
+            const req = index.getAll(timeRange);
+            req.onsuccess = () => {
+                resolve(req.result);
+            };
+            req.onerror = () => {
+                console.error(req.error);
+                reject([]);
+            };
+        })
+    }
+    
+    /**
+     * Gets all the sites in the db.
+     * @returns {Array} List of all the sites in the db.
+     */
+    async getAll() {
+        return new Promise((resolve, reject) => {
+            const store = app.DB.getSiteStore("readonly");
+            const req = store.getAll();
+            req.onsuccess = () => {
+                resolve(req.result);
+            };
+            req.onerror = () => {
+                console.error(req.error);
+                reject([]);
+            };
+        });
+    }
+
+    /**
+     * Deletes a site from the db using the id.
+     * @param {*} id Id of site to delete.
+     */
+    async delete(id) {
+        return new Promise((resolve, reject) => {
+            const store = app.DB.getSiteStore("readwrite");
+            req = store.delete(id);
+            req.onsuccess = () => {
+                resolve();
+            };
+            req.onerror = () => {
+                console.error(req.error);
+                reject();
+            };
+        });
+    }
+}
+
+class Site {
+    constructor(options = {}) {
+        this.id = 0;
+        Object.assign(this, options);
+    }
+
+    
+}
+
+class Article {
+    constructor(options = {}) {
+        this.id = 0;
+        Object.assign(this, options);
+    }
 }
 
 function generateSiteFromFeedObject(feedObj) {
@@ -58,7 +238,6 @@ async function getOrCreateSite(site) {
     return new Promise((resolve, reject) => {
         let store = getObjectStore(SITE_STORE_NAME, 'readwrite');
         let hashIndex = store.index('hash');
-        let id = 0;
         let req = hashIndex.get(site.hash);
         req.onsuccess = (evt) => {
             if ( typeof evt.target.result == 'undefined' ) {
@@ -71,8 +250,7 @@ async function getOrCreateSite(site) {
                 }
 
             } else {
-                id = evt.target.result.id;
-                resolve(id);
+                site.id = evt.target.result.id;
             }
         };
         req.onerror = (evt) => {
@@ -320,26 +498,6 @@ async function getArticlesInRange(store, start, end) {
                 resolve(result);
             }
         }
-    });
-}
-
-async function getSite(id, isHash) {
-    return new Promise((resolve, reject) => {
-        const store = getObjectStore(SITE_STORE_NAME, "readonly");
-        let req;
-        if ( isHash === undefined || !isHash ) {
-            req = store.get(id);
-        } else {
-            const index = store.index('hash');
-            req = index.get(id);
-        }
-        req.onsuccess = (event) => {
-            resolve(event.target.result);
-        };
-        req.onerror = (event) => {
-            console.error(event.target.error);
-            reject(null);
-        };
     });
 }
 
